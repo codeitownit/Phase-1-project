a64cdf89c689feb7402cb5bdae3e6cbbf3c0d80c
Everlyn <gachuieve32@gmail.com>
2023-10-15 15:52:58 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+//DOMContentLoaded event is fired
 document.addEventListener("DOMContentLoaded", ()=>{
     fetchAllArt();
     searchAllArt();
 })
+//Initiazing global variables
 let artData = [];
 let artId ;
 let searchData;
 let allSearchData;
 let info = [];
 let iiif_url = "https://www.artic.edu/iiif/2/";
+
+//fetching all data from the api endpoint
 function fetchAllArt(){
     fetch("https://api.artic.edu/api/v1/artworks?limit=60")
     .then((response)=>response.json())
@@ -16,15 +20,14 @@ function fetchAllArt(){
         iterateArtData();
     })
 }
-
+//iterate the data and return the iterator object and call displayAllArt function
 function iterateArtData(){
    artData.data.forEach(art =>{
-    console.log(art.title)
-    console.log(search.image_id)
     displayAllArt(art)
    })
 } 
 
+//display all art fetched on the page when loaded
 function displayAllArt(art){
     const div = document.querySelector("#all-art")
     const wrapper = document.createElement("div")
@@ -41,6 +44,8 @@ function displayAllArt(art){
     div.appendChild(wrapper);
     
 }
+
+//listens for search input changes and fetches search data
 function searchAllArt(){
         const form = document.querySelector("form");
         form.addEventListener("submit",(e)=>{
@@ -48,35 +53,32 @@ function searchAllArt(){
             const input = document.querySelector("#search").value
             const artName =input.split(" ").join("")
             const div = document.querySelector("#all-art")
-            div.innerHTML ="";
-            console.log(artName)
+            div.innerHTML ="";  //empties the dom element content
             fetch("https://api.artic.edu/api/v1/artworks/search?q="+artName)
             .then(response => response.json())
             .then(data =>{
                 allSearchData = data;
-                console.log(allSearchData.data)
                 allSearchData.data.forEach(item =>{
                     artId= item.id;
-                    console.log(artId)
                     fetchSearchArt(artId);
                 })
             })
             form.reset();
         });
 }
+//fetches search results using art Id
 function fetchSearchArt(artId){
     fetch("https://api.artic.edu/api/v1/artworks/"+artId)
     .then(response => response.json())
     .then(data =>{
         searchData = data
         info = searchData.data
-        console.log(info.image_id)
        displaySearchData(info)
 
         })
     
 }
-
+//updates the dom with the new search results
 function displaySearchData(info){
     const div = document.querySelector("#all-art")
     const wrapper = document.createElement("div")
