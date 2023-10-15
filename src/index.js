45fe2d3bfc54d5e1529967d79633ef9303151968
Everlyn <gachuieve32@gmail.com>
2023-10-15 22:03:52 +0300

    Update files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     searchAllArt();
 })
 //Initiazing global variables
+const div = document.querySelector("#all-art")
+let clickedArt;
 let artData = [];
 let artId ;
 let searchData;
@@ -29,7 +31,6 @@ function iterateArtData(){
 
 //display all art fetched on the page when loaded
 function displayAllArt(art){
-    const div = document.querySelector("#all-art")
     const wrapper = document.createElement("div")
     wrapper.className = "wrapper col-md-4";
     const image = document.createElement("img")
@@ -42,7 +43,45 @@ function displayAllArt(art){
     wrapper.appendChild(h6)
     wrapper.appendChild(p);
     div.appendChild(wrapper);
+    wrapper.addEventListener("click", ()=>{
+        clickedArt = art;
+        showClickedArt(art);
+    });
     
+}
+function showClickedArt(art){
+    div.innerHTML = "";
+    const clickedWrapper = document.createElement("div");
+    const clickedImage = document.createElement("img");
+    const clickedTitle = document.createElement("h6");
+    const hr1 = document.createElement("hr");
+    const hr2 = document.createElement("hr");
+    const hr3 = document.createElement("hr");
+    const artist = document.createElement("p");
+    artist.className = "artist-text";
+    const description = document.createElement("p");
+    const place = document.createElement("p");
+    const date = document.createElement("p");
+    const medium = document.createElement("p");
+    clickedWrapper.className = "container clicked-wrapper";
+    clickedImage.src = iiif_url + art.image_id + "/full/843,/0/default.jpg";
+    clickedTitle.textContent = art.title;
+    artist.textContent = art.artist_display;
+    description.innerHTML = `${art.description}`;
+    place.innerHTML = `<p><b>Place: </b> ${art.place_of_origin}</p>`
+    date.innerHTML = `<p><b>Date: </b>${art.date_display}</p>`;
+    medium.innerHTML = `<p><b>Medium: </b>${art.medium_display}</p>`;;
+    clickedWrapper.appendChild(clickedImage);
+    clickedWrapper.appendChild(clickedTitle);
+    clickedWrapper.appendChild(artist);
+    clickedWrapper.appendChild(description);
+    clickedWrapper.appendChild(hr1);
+    clickedWrapper.appendChild(place);
+    clickedWrapper.appendChild(hr2);
+    clickedWrapper.appendChild(date);
+    clickedWrapper.appendChild(hr3); 
+    clickedWrapper.appendChild(medium);
+    div.appendChild(clickedWrapper);
 }
 
 //listens for search input changes and fetches search data
@@ -52,7 +91,6 @@ function searchAllArt(){
             e.preventDefault();
             const input = document.querySelector("#search").value
             const artName =input.split(" ").join("")
-            const div = document.querySelector("#all-art")
             div.innerHTML ="";  //empties the dom element content
             fetch("https://api.artic.edu/api/v1/artworks/search?q="+artName)
             .then(response => response.json())
@@ -80,7 +118,6 @@ function fetchSearchArt(artId){
 }
 //updates the dom with the new search results
 function displaySearchData(info){
-    const div = document.querySelector("#all-art")
     const wrapper = document.createElement("div")
     wrapper.className = "wrapper col-md-4";
     const searchImage = document.createElement("img")
@@ -94,3 +131,4 @@ function displaySearchData(info){
     wrapper.appendChild(p);
     div.appendChild(wrapper);
 }
+
